34ce015046719a20a8d0cd14ea0d797e5ae622f8
srajan1122 <mahabala1122@gmail.com>
2019-10-26 15:59:21 +0530

    Bookingform and logout

--- login/models.py
+++ login/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    audi_choices = [
+        ('Auditorium','Auditorium'),
+        ('Place2','Place2'),
+        ('Place3','Place3')
+    ]
+    default_choice = [
+        ('Auditorium','Auditorium')
+    ]
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    place = models.CharField(max_length=100, choices = audi_choices,default=default_choice)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    status = models.BooleanField()
+
+    def __str__(self):
+        return self.name
