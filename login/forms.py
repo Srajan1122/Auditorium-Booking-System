f849d47020fa2ebe6bc702e5731167af9f0b1f1e
srajan1122 <mahabala1122@gmail.com>
2019-10-26 17:50:27 +0530

    booking form

--- login/forms.py
+++ login/forms.py
@@ -3,9 +3,54 @@ from django.forms import ModelForm
 from .models import Booking
 
 class BookingForm(ModelForm):
+    name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'type': 'text',
+                'class' : 'form-control',
+                'id' : 'validationTooltip01',
+                'placeholder' : 'Name',
+            }
+        )
+    )
+    email = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'type': 'email',
+                'class' : 'form-control',
+                'id' : 'validationTooltip02',
+                'placeholder' : 'Email',
+            }
+        )
+    )
+    date = forms.DateField(
+        widget=forms.TextInput(
+            attrs={
+                'type': 'date',
+                'class' : 'form-control',
+                'id' : 'validationTooltip03',
+                'placeholder' : 'Date',
+            }
+        )
+    )
+    start_time= forms.TimeField(
+        widget=forms.TextInput(
+            attrs={
+                'type': 'time',
+                'class' : 'form-control',
+                'id' : 'validationTooltip05',
+            }
+        )
+    )
+    end_time= forms.TimeField(
+        widget=forms.TextInput(
+            attrs={
+                'type': 'time',
+                'class' : 'form-control',
+                'id' : 'validationTooltip06',
+            }
+        )
+    )
     class Meta:
         model = Booking
-        widgets = {
-            'date': forms.DateInput(attrs={'class':'datepicker'}),
-        }
         fields = ('name','email','place','date','start_time','end_time')
--- login/models.py
+++ login/models.py
@@ -16,7 +16,7 @@ class Booking(models.Model):
     date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
-    status = models.BooleanField()
+    status = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
